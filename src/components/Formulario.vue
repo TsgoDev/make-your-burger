23832a901c3ef4c861411b86dcee69f6f7ad8d37
Tiago Soares <tsgo.soares@gmail.com>
2025-10-07 18:15:26 -0300

    feat: formulario de cadastro

---
+++ src/components/Formulario.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="form-container">
+        <p>Componente de mensagem</p>
+        <form id="burger-form" method="POST" @submit="createBurger">
+            <div class="input-container">
+                <label for="nome">Nome do cliente:</label>
+                <input type="text" id="nome" name="nome" placeholder="Digite seu nome" required>
+            </div>
+            <div class="input-container">
+                <label for="pao">Escolha o pão:</label>
+                <select name="pao" id="pao" v-model="pao">
+                    <option value="">Selecione o seu pão</option>
+                    <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
+                </select>
+            </div>
+            <div class="input-container">
+                <label for="carne">Escolha a carne do seu Burger:</label>
+                <select name="carne" id="carne" v-model="carne">
+                    <option value="">Selecione o tipo de carne</option>
+                    <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
+                </select>
+            </div>
+            <div class="input-container">
+                <label for="opecionais">Escolha os opcionais</label>
+                <div class="ckeckbox-container">
+                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                    <span>Salame</span>
+                    </input>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Formulario",
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <Banner/>
   <div class="main-container">
-    <h1>Monte seu Burger</h1>
-    <P>Formulário</P>
+    <Formulario />
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Banner.vue';
+import Formulario from '@/components/Formulario.vue';
 
 export default {
   nome: "Home",
   components: {
     Banner,
+    Formulario,
   }
 }
 </script>
