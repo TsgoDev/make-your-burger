ab68be61cd6238d56e2ab9a6165f6faae0073cea
Tiago Soares <tsgo.soares@gmail.com>
2025-10-07 11:32:48 -0300

    Criar componente Footer add no App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar />
+  <router-view />
+  <Footer/>
 </template>
 
-<style>
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
-</style>
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
