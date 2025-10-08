171a796b3990b64991d64142dbf52294bfb21bf0
Tiago Soares <tsgo.soares@gmail.com>
2025-10-08 15:13:38 -0300

    feat: adicionar msg de alerta para update delete

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,156 +1,180 @@
 <template>
-    <div id="burger-table">
-        <div>
-            <div id="burger-table-heading">
-                <div class="order-id">ID</div>
-                <div>Cliente</div>
-                <div>Pão</div>
-                <div>Carne</div>
-                <div>Opcionais</div>
-                <div>Ações</div>
-            </div>
-        </div>
-        <div id="burger-table-rows">
-            <div class="burger-table-row" v-for="burger in burgers" :key="burger_id">
-                <div class="order-number">{{ burger.id }}</div>
-                <div>{{burger.nome}}</div>
-                <div>{{ burger.pao }}</div>
-                <div>{{burger.pao}}</div>
-                <div>
-                    <ul>
-                    <li v-for="(opcional, index) in burger.opcionais" :key="index">
-                        {{opcional}}</li>
-                    </ul>
-                </div>
-                <div>
-                   <select name="status" class="status" @change="updateBurger($event, burger.id)">
-                    <option value="">Selecione</option>
-                    <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
-                        {{ s.tipo }}
-                    </option>
-                   </select>
-                   <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
-                </div>
-            </div>
-        </div>
+  <div id="burger-table">
+    <Message :msg="msg" v-show="msg" />
+    <div>
+      <div id="burger-table-heading">
+        <div class="order-id">ID</div>
+        <div>Cliente</div>
+        <div>Pão</div>
+        <div>Carne</div>
+        <div>Opcionais</div>
+        <div>Ações</div>
+      </div>
     </div>
+    <div id="burger-table-rows">
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger_id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>
+          <ul>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+              {{ opcional }}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <select
+            name="status"
+            class="status"
+            @change="updateBurger($event, burger.id)"
+          >
+            <option value="">Selecione</option>
+            <option
+              v-for="s in status"
+              :key="s.id"
+              :value="s.tipo"
+              :selected="burger.status == s.tipo"
+            >
+              {{ s.tipo }}
+            </option>
+          </select>
+          <button class="delete-btn" @click="deleteBurger(burger.id)">
+            Cancelar
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
+import Message from "./Message.vue";
 export default {
-    name: "Dashboard",
-    data() {
-      return {
-        burgers: null,
-        burger_id: null,
-        status: []
-      }
+  name: "Dashboard",
+  components: {
+    Message,
+  },
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+      msg: null,
+    };
   },
   methods: {
-     async getPedidos() {
-        const req = await fetch('http://localhost:3000/burgers');
-        const data = await req.json()
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+      const data = await req.json();
 
-        this.burgers = data
+      this.burgers = data;
 
-        // Resgatar status
-        this.getStatus();
-        
+      // Resgatar status
+      this.getStatus();
     },
     async getStatus() {
-        const req = await fetch('http://localhost:3000/status');
-        const data = await req.json()
+      const req = await fetch("http://localhost:3000/status");
+      const data = await req.json();
 
-        this.status = data;
+      this.status = data;
     },
     async deleteBurger(id) {
-        const req = await fetch(`http://localhost:3000/burgers/${id}`,{
-            method: "DELETE"
-        });
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE",
+      });
 
-        const res = await req.json();
+      const res = await req.json();
 
-        //msg
+      // colocar msg de alerta
+      this.msg = "Pedido removido com sucesso!";
 
-        this.getPedidos();
+      // limpar msg
+      setTimeout(() => (this.msg = ""), 3000);
+
+      this.getPedidos();
     },
     async updateBurger(event, id) {
+      const option = event.target.value;
 
-        const option = event.target.value;
+      const dataJson = JSON.stringify({ status: option });
 
-        const dataJson = JSON.stringify({status: option});
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "PATCH",
+        headers: { "Content-type": "application/json" },
+        body: dataJson,
+      });
 
-        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
-            method: "PATCH",
-            headers: {"Content-type": "application/json"},
-            body:dataJson
-        });
+      const res = await req.json();
 
-        const res = await req.json();
-    }
+      // colocar msg de alerta
+      this.msg = `Pedido N° ${res.id} foi atualizado para ${res.status}!`;
+
+      // limpar msg
+      setTimeout(() => (this.msg = ""), 3000);
+    },
   },
   mounted() {
     this.getPedidos();
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  #burger-table {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
+#burger-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
 
-  #burger-table-heading,
-  #burger-table-rows,
-  .burger-table-row {
-    display: flex;
-    flex-wrap: wrap;
-  }
+#burger-table-heading,
+#burger-table-rows,
+.burger-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
 
-  #burger-table-heading {
-    font-weight: bold;
-    padding: 12px;
-    border-bottom: 3px solid #333;
-  }
+#burger-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
 
-  .burger-table-row {
-    width: 100%;
-    padding: 12px;
-    border-bottom: 1px solid #CCC;
-  }
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
 
-  #burger-table-heading div,
-  .burger-table-row div {
-    width: 19%;
-  }
+#burger-table-heading div,
+.burger-table-row div {
+  width: 19%;
+}
 
-  #burger-table-heading .order-id,
-  .burger-table-row .order-number {
-    width: 5%;
-  }
+#burger-table-heading .order-id,
+.burger-table-row .order-number {
+  width: 5%;
+}
 
-  select {
-    padding: 12px 6px;
-    margin-right: 12px;
-  }
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+}
 
-  .delete-btn {
-    background-color: #222;
-    color:#fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
-  }
-  
-  .delete-btn:hover {
-    background-color: transparent;
-    color: #222;
-  }
+.delete-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+}
 
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
 </style>
